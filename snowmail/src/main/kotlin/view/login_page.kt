a55ad58dcc276fd2553e82016378423f24f31af8
SiyuHuang479 <128996046+SiyuHuang479@users.noreply.github.com>
2024-10-09 18:42:20 -0400

    edit login page

--- snowmail/src/main/kotlin/view/login_page.kt
+++ snowmail/src/main/kotlin/view/login_page.kt
@@ -1,31 +1,48 @@
 package ca.uwaterloo.view
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.ui.window.application
-
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.DragData
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.input.key.*
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.*
+import java.io.File
+import kotlin.math.sin
+
+import javax.imageio.ImageIO
+import androidx.compose.ui.graphics.toComposeImageBitmap
+
 
 
 @Composable
 fun loginPage() {
     Column (
-        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Job Hunting: Tough, \n But you Do Not Have \nto Do It Alone!")
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,) {
+            Text(
+                "Job Hunting: Tough, \n But you Do Not Have \nto Do It Alone!",
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 24.dp))
             loginForm()
     }
 }
@@ -34,9 +51,10 @@ fun loginPage() {
 fun loginForm() {
     Column(
         modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
-
     ) {
         loginWithAccount()
+        Divider()
+        // HorizontalDivider(thickness = 2.dp)
         loginWithGmail()
     }
 }
@@ -44,13 +62,23 @@ fun loginForm() {
 
 @Composable
 fun loginWithAccount() {
-    Column (modifier = Modifier.fillMaxSize(),
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(50.dp),
         verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
-        Text("Email Address")
-        OutlinedTextField(value = "email", onValueChange = {})
-        Text("Password")
-        OutlinedTextField(value = "email", onValueChange = {})
-        Text("Forgot Your Password?")
+
+        // email
+        Text("Email Address", modifier = Modifier.align(Alignment.Start))
+        var email by remember { mutableStateOf("") }
+        OutlinedTextField(value = email, onValueChange = { email = it }, modifier = Modifier.fillMaxWidth().align(Alignment.Start), singleLine = true)
+
+        // password
+        Text("Password", modifier = Modifier.align(Alignment.Start))
+        var password by remember { mutableStateOf("") }
+        OutlinedTextField(value = password, onValueChange =  {password = it}, modifier = Modifier.fillMaxWidth().align(Alignment.Start), singleLine = true)
+
+        // forgot your password?
+        ClickableText(AnnotatedString("Forgot Your Password?"), onClick = {})
+
         Button(onClick = { onClick() }) {
             Text("Sign In")
         }
@@ -62,11 +90,20 @@ fun loginWithAccount() {
 
 @Composable
 fun loginWithGmail() {
-    Column(modifier = Modifier.fillMaxSize(),
+    Column(
        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         Text("Or log in with")
+        val iconpath = File("gmail_icon.png")
+
+        val iconimage = ImageIO.read(iconpath)
+        val iconbitmap = iconimage.toComposeImageBitmap()
         Button(onClick = { onClick() }) {
-            Text("Gmail")
+            Row (horizontalArrangement = Arrangement.Center){
+                Image(iconbitmap, "gmail")
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("Gmail")
+            }
+
         }
     }
 }
