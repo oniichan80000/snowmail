d4c9f316fe77f57c844a07e15f291b4988918593
SiyuHuang479 <128996046+SiyuHuang479@users.noreply.github.com>
2024-10-31 15:58:18 -0400

    Service/EmailSendingService.kt, controller/SendEmailController.kt

--- snowmail/src/main/kotlin/controller/SendEmailController.kt
+++ snowmail/src/main/kotlin/controller/SendEmailController.kt
@@ -1,2 +1,19 @@
 package controller
 
+import model.email
+import service.sendEmail
+
+fun send_email(
+    senderEmail: String,
+    password: String,
+    recipient: String,
+    subject: String, text: String,
+    fileURLs: List<String>,
+    fileNames: List<String>) {
+    val Email = email(senderEmail, password, recipient, subject, text, fileURLs, fileNames)
+    sendEmail(Email)
+}
+
+
+
+
