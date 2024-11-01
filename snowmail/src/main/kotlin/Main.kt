211f43b6b40d4cea4d7947711944b65b07803079
SiyuHuang479 <128996046+SiyuHuang479@users.noreply.github.com>
2024-10-31 22:33:20 -0400

    solve merge

--- snowmail/src/main/kotlin/Main.kt
+++ snowmail/src/main/kotlin/Main.kt
@@ -7,13 +7,9 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import ca.uwaterloo.view.*
-import controller.configureRouting
 import integration.OpenAIClient
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import kotlin.concurrent.thread
 import service.EmailGenerationService
 
 fun main() {
@@ -25,11 +21,11 @@ fun main() {
     val emailGenerationService = EmailGenerationService(openAIClient)
 
     // Start the Ktor server in a separate thread
-    thread {
-        embeddedServer(Netty, port = 8080) {
-            configureRouting(emailGenerationService)
-        }.start(wait = true)
-    }
+//    thread {
+//        embeddedServer(Netty, port = 8080) {
+//            configureRouting(emailGenerationService)
+//        }.start(wait = true)
+//    }
 
 
     application {
@@ -46,14 +42,15 @@ fun websitePage() {
     var currentPage by remember { mutableStateOf("welcome") }
 
     when (currentPage) {
-        "login" -> loginPage ({ currentPage = "signup" }, {currentPage = "homepage"})
-        "signup" -> SignUpPage ({ currentPage = "login"}, { currentPage = "homepage"})
+        "login" -> loginPage ({ currentPage = "signup" }, {currentPage = "profilePage"})
+        "signup" -> SignUpPage ({ currentPage = "login"}, { currentPage = "login"})
         "welcome" -> WelcomePage ({ currentPage = "signup"}, {currentPage = "login"}, {currentPage = "welcome1"}, {currentPage = "emailgeneration"})
         "welcome1" -> WelcomePage1 ({ currentPage = "signup"}, {currentPage = "login"}, {currentPage = "welcome2"}, {currentPage = "welcome3"}, {currentPage = "welcome4"})
         "welcome2" -> WelcomePage2 ({ currentPage = "signup"}, {currentPage = "login"}, {currentPage = "welcome3"})
         "welcome3" -> WelcomePage3 ({ currentPage = "signup"}, {currentPage = "login"}, {currentPage = "welcome4"})
         "welcome4" -> WelcomePage4 ({ currentPage = "signup"}, {currentPage = "login"})
         "profilePage" -> ProfilePage()
+        "emailgeneration" -> EmailGenerationPage()
     }
 }
 
