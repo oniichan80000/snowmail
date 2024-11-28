b17294dfdd6f68cc1d8b95fb8eab0484676c4b15
Yu Xiao <y243xiao@uwaterloo.ca>
2024-11-28 05:00:23 -0500

    delete comments

--- snowmail/build.gradle.kts
+++ snowmail/build.gradle.kts
@@ -78,8 +78,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
 
    // implementation ("androidx.compose.ui:ui:1.7.5")
-    //implementation("com.github.vanpra.compose-multiplatform-dialogs:core:0.6.0")
-    //implementation ("com.google.accompanist:accompanist-flowlayout:0.24.13-rc")
 
 
 }
